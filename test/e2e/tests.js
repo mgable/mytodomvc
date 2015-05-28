b848bac312b55cceb51a251c275abab69c82fa8c
Mark Gable <mgable43@gmail.com>
2015-05-28 11:50:30 -0700

    added first two tests

--- test/e2e/tests.js
+++ test/e2e/tests.js
@@ -7,14 +7,22 @@ beforeEach(
 	}
 );
 
-describe("Executing tests", function(){
-	it("should set up the tests", function(){
-		console.info("tests set up");
+describe("Todo MVC", function(){
+	it("should enter a todo", function(){
+		var inputField = element(by.model("newTodo"));
+		inputField.sendKeys("write a new review");
+		inputField.getAttribute('value').then(function(inputtext){
+			expect(inputtext).toEqual("write a new review");
+		});
 	});
 
-	describe("the application", function(){
-		it("should work", function(){
-			expect(true).toBe(true);
+	it("should save a todo", function(){
+		var inputField = element(by.model("newTodo")),
+			todos = element.all(by.repeater("todo in todos"));
+		inputField.sendKeys("write a new review");
+		inputField.sendKeys(protractor.Key.ENTER);
+		todos.first().getText().then(function(text){
+			expect(text).toEqual("write a new review");
 		});
 	});
 })
