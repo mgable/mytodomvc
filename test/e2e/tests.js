a2062b2d9f2cd740fc31f13a5f8d5bdb3aebb6f8
Mark Gable <mgable@marketshare.com>
2015-07-02 07:41:31 -0700

    three

--- test/e2e/tests.js
+++ test/e2e/tests.js
@@ -40,6 +40,42 @@ describe("Todo MVC", function(){
 		});
 	});
 
+	//3.0
+	it("should edit a todo", function(){
+		var todos = element.all(by.repeater("todo in todos")), 
+			firstTodo = todos.first(),
+			firstTodoInput = firstTodo.element(by.css("label")),
+			title;
+
+		todos.count().then(function(count){
+			if (count){
+				firstTodoInput.getText().then(function(text){
+					title = text;
+
+					browser.actions().doubleClick(firstTodo).perform();
+					hasClass(firstTodo,"editing").then(function(hasClass){
+						expect(hasClass).toBe(true); //3.1
+					});
+
+					browser.actions().sendKeys(" -xxx").perform();
+					browser.actions().sendKeys(protractor.Key.ESCAPE).perform();
+					firstTodoInput.getText().then(function(text){
+						expect(text).toEqual(title); //3.2
+					});
+
+					browser.actions().doubleClick(firstTodo).perform();
+					browser.actions().sendKeys(" -new").perform();
+					browser.actions().sendKeys(protractor.Key.ENTER).perform();
+					firstTodoInput.getText().then(function(newTitle){
+						expect(newTitle).toEqual(title + " -new"); //3.3
+					});
+
+				});
+			} else {
+				console.info("there are no todos");
+			}
+		});
+	});
 })
 
 		
